ec0615ba67dbf7e34675130fe19fc29f1bb01a2c
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2025-10-20 21:17:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,3 @@ import streamlit as st
 # 2. On utilise une fonction de Streamlit pour afficher du texte
 # st.title() affiche le texte comme un titre principal
 st.title("Bonjour à tous !")
-
-d = {'Col1': ['toi', 'moi'], 'Col2': [1, 2]}
-
-st.dataframe(d)
